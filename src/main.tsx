3118301e7d4d139d4857e6484c2f3409ff99631a
YeongKwonMo <momoci99@gmail.com>
2026-04-22 00:27:33 +0900

    fix:gtm, Analytics 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 
@@ -7,6 +8,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>
 );
